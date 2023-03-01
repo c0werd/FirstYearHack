bd57a1c8a570067a244c333cbdcf77698c9f1a3a
c0werd <james.sy.coward@gmail.com>
2023-03-01 17:15:26 +0000

    sheeeeeeeeesh
    Co-authored-by: Diwas Rai <diwasrai38@yahoo.com>

---
+++ Challenge2.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Challenge2 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("challenge_2_and_3.txt");
+            Scanner sc = new Scanner(file);
+            Challenge2 challenge2 = new Challenge2();
+
+            for(int i = 0; i <=4; i++){
+                System.out.println(challenge2.getBotName(sc));
+                System.out.println(challenge2.getInitSecurities(sc));
+                challenge2.getDivisible(sc);
+                challenge2.getBotOne(sc);
+                challenge2.getNotBotOne(sc);
+            }
+
+            sc.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        
+    }
+
+    //
+    public String getBotName(Scanner sc){
+       String line = sc.nextLine();
+       String name = line.substring(4);
+       
+       return name;
+    }
+
+    public ArrayList<Integer> getInitSecurities(Scanner sc){
+        String line = sc.nextLine();
+        String securitiesString = line.substring(19);
+        String[] arr = securitiesString.split(",");
+        ArrayList<Integer> securities = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].trim();
+            securities.add(Integer.valueOf(arr[i]));
+        }
+
+        return securities;
+    }
+
+    public void getDivisible(Scanner sc){
+        String divisible = sc.nextLine();
+        
+
+    }
+
+    public void getBotOne(Scanner sc){
+        String tradeTo = sc.nextLine();
+
+    }
+
+    public void getNotBotTwo(Scanner sc){
+        String notTradeto = sc.nextLine();
+
+    }
+}
